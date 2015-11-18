5ab852c11d8dee4b03a732b821b1e2daa516e5f4
cielo <cielo33456@nctu.edu.tw>
2015-11-18 10:13:41 +0800

    update sort

--- sort.py
+++ sort.py
@@ -10,7 +10,6 @@ def bubble(sortlist):
     for times in range(1, length):
 	for flag in range(1, length-times+1):
 	    if sortlist[flag-1] > sortlist[flag]:
-		sortlist[flag-1], sortlist[flag] = sortlist[flag], sortlist[flag-1]
-#		swap(sortlist[flag-1],sortlist[flag] )
-#               print("? ")
+#		sortlist[flag-1], sortlist[flag] = sortlist[flag], sortlist[flag-1]
+		sortlist[flag-1], sortlist[flag] = swap(sortlist[flag-1],sortlist[flag] )
     return
